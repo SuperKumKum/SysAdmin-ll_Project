38fffa0dc3e17934039b08b3e0562f06e17fb224
SuperKumKum <gossiaux_chris@live.be>
2017-05-16 21:56:26 +0200

    modification serv web - ajout ssl

--- apache/sites/b2b/index.php
+++ apache/sites/b2b/index.php
@@ -1,8 +1,7 @@
  <?php
     $username = "wt7";
-    $password = "myPassword";
+    $password = "password";
     $hostname = "db"; 
-
     //connection to the database
     $dbhandle = mysql_connect($hostname, $username, $password) 
     or die("Unable to connect to MySQL");
